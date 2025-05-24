2ed41a65b690aba105538ba1c205e2fc55049bd7
Ilya Balykov <balykov.ilya.88@gmail.com>
2025-05-24 18:57:47 +0300

    feat: add add() method to MyHashSet

---
+++ src/MyHashSet.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class MyHashSet <E> implements Collection <E>{
+
+    private ArrayList <E> newArray;
+
+    public MyHashSet() {
+        this.newArray = new ArrayList<>();
+    }
+
+    @Override
+    public boolean add(E someObj) {
+        if(newArray.contains(someObj)) {
+            return false;
+        } else {
+            newArray.add(someObj);
+            return true;
+        }
+    }
+}
