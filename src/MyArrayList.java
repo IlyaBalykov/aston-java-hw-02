03d409f3b5ab5a3a69607a84ad138ddca40b20e0
Ilya Balykov <balykov.ilya.88@gmail.com>
2025-05-24 22:28:40 +0300

    feat: add add() method to MyArrayList

---
+++ src/MyArrayList.java
@@ -0,0 +1,42 @@
+import java.sql.Array;
+import java.util.Collection;
+
+public class MyArrayList <E> implements Collection <E> {
+
+    private Object[] newArray;
+    private int size;
+
+    public MyArrayList(int initialSize) {
+        if (initialSize >= 0) {
+            this.newArray = new Object[initialSize];
+        }
+        this.size = 0;
+    }
+
+    public boolean add(E obj) {
+        if (size == newArray.length) {
+            Object[] tmpArray = new Object[(int) ((size * 1.5) + 1)];
+            System.arraycopy(newArray, 0, tmpArray, 0, size);
+            newArray = tmpArray;
+            newArray[size] = obj;
+            size++;
+            return true;
+        } else {
+            newArray[size] = obj;
+            size++;
+            return true;
+        }
+    }
+
+    public boolean addAll() {
+
+    }
+
+    public E get() {
+
+    }
+
+    public boolean remove() {
+
+    }
+}
