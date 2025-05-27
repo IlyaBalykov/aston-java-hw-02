802ae7e1042c3371ae1e4055ecef621eff4a8ddd
Ilya Balykov <balykov.ilya.88@gmail.com>
2025-05-27 22:10:10 +0300

    fix: change newArray variable type from Object[] to E[]

--- src/MyHashSet.java
+++ src/MyHashSet.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class MyHashSet <E> implements Collection <E>{
+public class MyHashSet <E> implements Collection <E> {
 
     private ArrayList <E> newArray;
 
